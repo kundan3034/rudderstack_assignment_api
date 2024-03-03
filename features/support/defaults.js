9b07ec92d65d0ab6af5b2a3364622d3ebc147118
Kundan Kumar <kundan@hackerearth.com>
2024-03-03 22:11:59 +0530

    handle baseurl

--- features/support/defaults.js
+++ features/support/defaults.js
@@ -5,9 +5,10 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'prod'}` });
 module.exports = {
   EMAIL: process.env.EMAIL,
   PASSWORD: process.env.PASSWORD,
+  BASEURL: process.env.BASEURL || 'https://api.rudderstack.com', // Default base URL
 };
 
 Before(() => {
-  request.setBaseUrl('https://api.rudderstack.com');
+  request.setBaseUrl(module.exports.BASEURL);
   settings.setReporterAutoRun(false);
 });
